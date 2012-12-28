0cee355113f0004c84ccbea948e70c30257b98b5
Sahil Bajaj <sahil29@gmail.com>
2012-12-28 22:27:08 +0530

    Refactored feed sorter

--- assets/js/feedsorter.js
+++ assets/js/feedsorter.js
@@ -1,80 +1,60 @@
-/*
-*  How to load a feed via the Feeds API.
-*/
-google.load("feeds", "1")
+(function ($, google) {
 
-// Our callback function, for when a feed is loaded.
-function feedLoaded(result) {
-	if (!result.error) {
-		// Grab the container we will put the results into
-		// var blogroll = document.getElementById("blogroll")
-		//container.innerHTML = '';
+	// Load Google Feed API
+	google.load("feeds", "1")
 
-		// Loop through the feeds, putting the titles onto the page.
-		// Check out the result object for a list of properties returned in each entry.
-		// http://code.google.com/apis/ajaxfeeds/documentation/reference.html#JSON
-//    for (var i = 0; i < result.feed.entries.length; i++) {
-			var entry = result.feed.entries[0];
-			var div = document.createElement("div");
-		console.log(result);
-			div.appendChild(document.createTextNode(entry.title + new Date(entry.publishedDate).getTime()));
-			container.appendChild(div);
-	//  }
+	function sortFeeds() {
+
+		var deferreds = []
+
+		// Get Feed URLs from data attributes in the list of links
+		var feeds = $.map($('.blogroll').find('a'), function (element) {
+			return $(element).data('feed')
+		})
+
+		$(feeds).each(function (index, feedUrl) {
+
+			deferreds[index] = $.Deferred()
+
+			var feed = new google.feeds.Feed(feedUrl)
+			feed.setResultFormat(google.feeds.Feed.JSON_FORMAT)
+
+			feed.load(function (result) {
+				// Augment the object to make the last published date
+				// easily accessible
+				result.feed.lastPublished = new Date(result.feed.entries[0].publishedDate)
+				deferreds[index].resolve(result.feed)
+			})
+		})
+
+		// Wait for all feeds to finish loading
+		$.when.apply($, deferreds).done(function () {
+
+			var htmlResult = []
+
+			// Sort feeds by last published date
+			var sortedFeeds = $.makeArray(arguments).sort(function (a, b) {
+
+				return new Date(b.lastPublished).getTime() - new Date(a.lastPublished).getTime()
+			})
+
+			// Generate HTML list to display on the page
+			$(sortedFeeds).each(function (index, value) {
+
+				htmlResult.push('<li><a href="' + value.link +
+					'" title="' + value.author +
+					'" data-feed="' + value.feedUrl +
+					'">' + (value.title || value.link) +
+					'</a> <time datetime="' + value.lastPublished +
+					'">' + moment(value.lastPublished).fromNow() +
+					'</time></li>')
+			})
+
+			$('.blogroll').html(htmlResult.join(''))
+			console.log('Done!');
+		})
 	}
-}
 
-function feedSorter(a, b) {
-	return new Date(b.lastPublished).getTime() - new Date(a.lastPublished).getTime()
-}
-
-function loadFeeds() {
-	var feeds = []
-	var deferreds = []
-	$('.blogroll').find('a').each(function (index, value) {
-		feeds.push($(value).data('feed'))
-	})
-
-	$(feeds).each(function (index, value) {
-		deferreds[index] = $.Deferred()
-
-		var feed = new google.feeds.Feed(feeds[index])
-		feed.setResultFormat(google.feeds.Feed.JSON_FORMAT)
-
-		feed.load(function (result) {
-			result.feed.lastPublished = new Date(result.feed.entries[0].publishedDate)
-			deferreds[index].resolve(result.feed)
-		})
-	})
-
-	/*for (var i = 0; i < feeds.length; i++) {
-		var feed = new google.feeds.Feed(feeds[i])
-		var deferred = $.Deferred()
-		var index = i
-		deferreds[index] = $.Deferred()
-		var dfd = deferreds[index]
-		feed.setResultFormat(google.feeds.Feed.JSON_FORMAT)
-
-		results = [];
-
-		feed.load(function (result) {
-			dfd.resolve(result.feed.entries[0])
-			// var entry = result.feed.entries[0]
-			// console.log(entry)
-			// results.push(entry)
-		})*/
-
-		// results.sort(feedSorter)
-	$.when.apply($, deferreds).done(function () {
-		var sortedFeeds = $.makeArray(arguments).sort(feedSorter)
-		var html = []
-		$(sortedFeeds).each(function (index, value) {
-			 html.push('<li><a href="' + value.link + '" title="' + value.author + '" data-feed="' + value.feedUrl + '">' + (value.title || value.link) + '</a> <time datetime="' + value.lastPublished + '">' + moment(value.lastPublished).fromNow() + '</time></li>')
-		})
-
-		$('.blogroll').html(html.join(''))
-		console.log('Done!');
-	})
-/*	}*/
-}
-
-google.setOnLoadCallback(loadFeeds)
+	// Register Feed API callback
+	google.setOnLoadCallback(sortFeeds)
+})(jQuery, google);
