4fc365fe57a55a45548fce6e41b92afe34adc84b
Sahil Bajaj <sahil29@gmail.com>
2015-07-06 00:15:32 +0800

    Replace Rakefile with new_post.sh
    - That's all I was using the Rakefile for anyway

---
+++ new_post.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+echo -n "Post title: "
+read post_title
+
+filename=`date +%F`-`echo -n $post_title | \
+	tr '[:upper:]' '[:lower:]' | \
+	sed s/\ /-/g`.md
+
+cat > _posts/$filename << EOF
+---
+layout: post
+date: `date +%FT%T%z`
+title: $post_title
+---
+EOF
